952cefc4c2f5279b28704fa60bb65782ebc42752
CodeCaven <simon.j.caven@gmail.com>
2023-04-16 12:20:00 +0000

    Maps adding major cities

--- public/javascripts/control.js
+++ public/javascripts/control.js
@@ -7,3 +7,75 @@ function private(){
     var new_path = "http://" + window.location.hostname + ":" + window.location.port + "/privatehealth";
     window.location.assign(new_path);
 }
+
+
+const cities = [  {    
+            city: "Sydney",    
+            location: [151.2093, -33.8688]
+        },
+        {
+            city: "Melbourne",
+            location: [144.9631, -37.8136]
+        },
+        {
+            city: "Brisbane",
+            location: [153.0251, -27.4698]
+        },
+        {
+            city: "Perth",
+            location: [115.8570, -31.9535]
+        },
+        {
+            city: "Adelaide",
+            location: [138.6007, -34.9285]
+        },
+        {
+            city: "Newcastle",
+            location: [151.7500, -32.9167]
+        },
+        {
+            city: "Canberra",
+            location: [149.1300, -35.2809]
+        },
+        {
+            city: "Sunshine Coast",
+            location: [153.0667, -26.6500]
+        },
+        {
+            city: "Townsville",
+            location: [146.8169, -19.2580]
+        },
+        {
+            city: "Cairns",
+            location: [145.7781, -16.9186]
+        },
+        {
+            city: "Darwin",
+            location: [130.8456, -12.4634]
+        },
+        {
+            city: "Alice Springs",
+            location: [133.8807, -23.6980]
+        },
+        {
+            city: "Hobart",
+            location: [147.3250, -42.8806]
+        },
+        {
+            city: "Launceston",
+            location: [147.1447, -41.4419]
+        },
+        {
+            city: "Bendigo",
+            location: [144.2833, -36.7570]
+        },
+        {
+            city: "Exmouth",
+            location: [114.1667, -21.9333]
+        }
+        ];
+
+
+
+
+
