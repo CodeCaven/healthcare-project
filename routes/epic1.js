f5eab6c8311b33f720c27f5a82d155edd388003e
CodeCaven <simon.j.caven@gmail.com>
2023-05-16 13:33:19 +0000

    Footer and nav fixes

--- routes/epic1.js
+++ routes/epic1.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('healthguidelines', { title: 'Express' });
+  res.render('healthguidelines2', { title: 'Express' });
 });
 
 module.exports = router;
