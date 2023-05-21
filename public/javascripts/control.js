d34122da97b9a7cfe1d279a7efb0a6c31df6ae13
CodeCaven <simon.j.caven@gmail.com>
2023-05-21 16:12:54 +0000

    Layout and UI fixes all epics

--- public/javascripts/control.js
+++ public/javascripts/control.js
@@ -42,6 +42,20 @@ function reveal() {
     }
   }
 
+  function revealX() {
+    var reveals = document.querySelectorAll(".revealX");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
 
 const cities = [  {    
             city: "Sydney",    
--- routes/epic1.js
+++ routes/epic1.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('healthguidelines2', { title: 'Express' });
+  res.render('healthguidelines3', { title: 'Express' });
 });
 
 module.exports = router;
